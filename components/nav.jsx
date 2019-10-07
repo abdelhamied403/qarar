ab051e661b18aaa8875e31ed1d4104d819e2cca7
Mohamed Khalil <arabscimitar@gmail.com>
2019-10-07 04:31:10 +0200

    add linting

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,24 +1,19 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
 const links = [
   { href: 'https://zeit.co/now', label: 'ZEIT' },
   { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+];
 
 const Nav = () => (
   <nav>
     <ul>
       <li>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
+        <Link href="/">Home</Link>
       </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
+      {links.map(({ href, label }) => (
+        <li key={href}>
           <a href={href}>{label}</a>
         </li>
       ))}
@@ -51,6 +46,6 @@ const Nav = () => (
       }
     `}</style>
   </nav>
-)
+);
 
-export default Nav
+export default Nav;
