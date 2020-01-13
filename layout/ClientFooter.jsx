75b11baf3e183ea447b97f1ff82f0bf8750a3cc7
Dina Tahoun <dinatahoun992@gmail.com>
2020-01-14 01:24:21 +0200

    landing

--- layout/ClientFooter.jsx
+++ layout/ClientFooter.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { Container, Button, Col, Row } from 'reactstrap';
+import Iframe from 'react-iframe';
 
 import './client.css';
 
@@ -11,21 +13,118 @@ const propTypes = {
 const defaultProps = {};
 
 const ClientFooter = ({ isAuthentcated }) => (
-  <div
-    className={isAuthentcated ? 'client-footer user-loggedin' : 'client-footer'}
-  >
-    <img src="/static/img/brand/logo-footer.svg" alt="qarar" />
-    <h3>قرار - وزارة الشؤون البلدية والقروية</h3>
-    <div className="footer-links">
-      <Link href="/about">
-        <a>عن قرار</a>
-      </Link>
-      <Link href="/faqs">
-        <a>أسئلة شائعة</a>
-      </Link>
-      <Link href="/contact-us">
-        <a> اتصل بنا</a>
-      </Link>
+  <div className={isAuthentcated ? ' user-loggedin' : 'newFooter'}>
+    <div className="upperFooter">
+      <a href="">
+        <img src="/static/img/interactive/fb.svg" alt="" />
+      </a>
+      <a href="">
+        <img src="/static/img/interactive/twitter.svg" alt="" />
+      </a>
+      <a href="">
+        <img src="/static/img/interactive/linkedin.svg" alt="" />
+      </a>
+      <a href="">
+        <img src="/static/img/interactive/instagram.svg" alt="" />
+      </a>
+    </div>
+    <div className="footer">
+      <Container>
+        <Row>
+          <Col md="2" sm="6" className="d-flex flex-column">
+            <h5>خدمات الوزارة</h5>
+            <Link>
+              <a href="">نبذة عن الوزارة</a>
+            </Link>
+            <Link>
+              <a href="">كلمة عن الوزير</a>
+            </Link>
+            <Link>
+              <a href="">الهيكل التنظيمي</a>
+            </Link>
+            <Link>
+              <a href="">الوكالات</a>
+            </Link>
+            <Link>
+              <a href="">المساهمة في 2030</a>
+            </Link>
+          </Col>
+          <Col md="2" sm="6" className="d-flex flex-column">
+            <h5>المركز الأعلامي</h5>
+            <Link>
+              <a href="">اخر الأخبار</a>
+            </Link>
+            <Link>
+              <a href="">الفاعاليات</a>
+            </Link>
+            <Link>
+              <a href="">الإعلانات</a>
+            </Link>
+            <Link>
+              <a href="">الوسائط</a>
+            </Link>
+            <Link>
+              <a href="">التعاميم</a>
+            </Link>
+          </Col>
+          <Col md="2" sm="6" className="d-flex flex-column">
+            <h5>البيانات المفتوحة</h5>
+            <Link>
+              <a href="">الأمانات و البلديات</a>
+            </Link>
+            <Link>
+              <a href="">البيانات المفتوحة</a>
+            </Link>
+            <Link>
+              <a href="">برامج الوزارة</a>
+            </Link>
+            <Link>
+              <a href="">الأنظمة و اللوائح</a>
+            </Link>
+            <Link>
+              <a href="">التقارير و الأحصائات</a>
+            </Link>
+            <Link>
+              <a href="">الأسئلة الشائعة</a>
+            </Link>
+          </Col>
+          <Col md="2" sm="6" className="d-flex flex-column">
+            <h5>تواصل معنا</h5>
+            <p>:الهاتف +966114569999</p>
+            <p>:البريد الإلكتروني p.r@momra.gov.sa</p>
+          </Col>
+          <Col md="2" sm="6" className="d-flex flex-column">
+            <h5>طريق الملك عبدالله - الرياض</h5>
+            <p>
+              الذهاب للموقع
+              <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
+            </p>
+          </Col>
+          <Col md="2" sm="6" className="d-flex flex-column newMap">
+            <Iframe
+              url="https://www.google.com/maps/embed?pb=!1m17!1m8!1m3!1d7247.823198314918!2d46.665416!3d24.729915!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m3!3m2!1d24.729915!2d46.665416!4m0!5e0!3m2!1sen!2seg!4v1578950123098!5m2!1sen!2seg"
+              width="100%"
+              height="200px"
+              id="myId"
+              className="myClassname"
+              display="initial"
+              position="relative"
+            />
+          </Col>
+        </Row>
+        <div className="bottomFooter d-flex justify-content-between align-items-center">
+          <p>©جميع الحقوق محفوظة وزارة الشئون البلدية والقروية - 2019</p>
+          <div className="d-flex align-items-center">
+            <p>
+              لتصفح الموقع بشكل جيد الرجاء استخدام الإصدارات الاخيرة من
+              المتصفحات التالية
+            </p>
+            <img src="/static/img/interactive/browser1.svg" alt="" />
+            <img src="/static/img/interactive/browser2.svg" alt="" />
+            <img src="/static/img/interactive/browser3.svg" alt="" />
+          </div>
+        </div>
+      </Container>
     </div>
   </div>
 );
--- views/Pages/Landing/Landing.jsx
+++ views/Pages/Landing/Landing.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Button, Col, Row, Media } from 'reactstrap';
+import { Container, Button, Col, Row, Media, ButtonGroup } from 'reactstrap';
 import Link from 'next/link';
 import moment from 'moment';
 
@@ -95,6 +95,11 @@ const Landing = () => {
   return (
     <div className="rtl newUILanding">
       <div className="header header-image-bg">
+        <img
+          src="/static/img/interactive/hero.svg"
+          alt=""
+          className="heroPattern"
+        />
         <Container>
           <section className="section-flex">
             <h2>شارك بصنع القرار</h2>
@@ -156,148 +161,241 @@ const Landing = () => {
           </section>
         </Container>
       </div>
-      <section className="social-sharing p100">
-        <Container>
-          <h2 className="text-center header">
-            المشاركة المجتمعية…من الأكثر تأثيراً؟
-          </h2>
-          <Row>
-            <Col xs="12" md="6" lg="4">
-              <CardPoints
-                avatar="/static/img/avatar.png"
-                // avatar={mostActiveUserAword.picture}
-                name={
-                  mostActiveUserAword.full_name || mostActiveUserAword.username
-                }
-                points={mostActiveUserAword.awards}
-                uid={mostActiveUserAword.uid}
-                number={mostActiveUserAword.awards}
-                icon="/static/img/trophy-icon.svg"
-              />
-            </Col>
-            <Col xs="12" md="6" lg="4">
-              <CardPoints
-                avatar="/static/img/avatar.png"
-                // avatar={mostActiveUserComment.picture}
-                name={
-                  mostActiveUserComment.full_name ||
-                  mostActiveUserComment.username
-                }
-                points={mostActiveUserAword.awards}
-                uid={mostActiveUserComment.uid}
-                number={mostActiveUserComment.comments}
-                icon="/static/img/Icon - most active - views Copy 2.svg"
-              />
-            </Col>
-            <Col xs="12" md="6" lg="4">
-              <CardPoints
-                avatar="/static/img/avatar.png"
-                // avatar={mostActiveUserLike.picture}
-                name={
-                  mostActiveUserLike.full_name || mostActiveUserLike.username
-                }
-                points={mostActiveUserAword.awards}
-                uid={mostActiveUserLike.uid}
-                number={mostActiveUserLike.likes}
-                icon="/static/img/Icon - most active - views Copy.svg"
-              />
-            </Col>
-            <Col xs="12" md="6">
-              <CardInfo
-                description="عدد مستخدمي المنصة الحالي"
-                info="مستخدم"
-                number={userCount}
-                icon="/static/img/Icon - draft activity - users.svg"
-              />
-            </Col>
-            <Col xs="12" md="6">
-              <CardInfo
-                description="عدد المسودات التي تمت مناقشتها"
-                info="مسودة"
-                number={draftCount}
-                icon="/static/img/discussed drafts card.svg"
-              />
-            </Col>
-          </Row>
-        </Container>
-      </section>
 
       <section className="activities">
         <Container>
-          <h2 className="text-center header">نشاط المسودات</h2>
+          <div className="d-flex justify-content-between align-items-center mb-5">
+            <h2 className="text-center header">نشاط المسودات</h2>
+            <ButtonGroup>
+              <Button>اكثر نشاطا هذا الاسبوع</Button>
+              <Button className="active">نشرت حديثا</Button>
+              <Button>تغلق قريبا</Button>
+            </ButtonGroup>
+          </div>
           <Row>
-            <Col xs="12" md="6" lg="4">
-              <CardDescription
-                header="المسودات الاكثر نشاطا هذا الاسبوع"
-                type="social"
-                arrayOfContnt={activeDrafts.slice(0, 2).map(draft => ({
-                  header: draft.title,
-                  link: `/draft-details/${draft.id}`,
-                  social: [
-                    {
-                      icon: '/static/img/Icon - most active - views Copy 2.svg',
-                      number: draft.comments
-                    },
-                    {
-                      icon: '/static/img/Icon - most active - views Copy.svg',
-                      number: draft.likes
-                    },
-                    {
-                      icon: '/static/img/Icon - most active - views.svg',
-                      number: 2000
-                    }
-                  ]
-                }))}
-              />
+            <Col xs="12" md="6" lg="3">
+              <div className="oneActivity">
+                <h4>إشتراطات المباني التجارية</h4>
+                <p>
+                  <img src="/static/img/interactive/calendar.svg" alt="" />
+                  يغلق التصويت بتاريخ 01-01-2020
+                </p>
+              </div>
             </Col>
-            <Col xs="12" md="6" lg="4">
-              <CardDescription
-                header="مسودات نشرت حديثاً"
-                type="b"
-                arrayOfContnt={drafts.map(draft => ({
-                  header: draft.title,
-                  link: `/draft-details/${draft.id}`,
-                  description: [
-                    {
-                      icon: '/static/img/Icon - most active - views Copy 3.svg',
-                      text: `يغلق التصويت بتاريخ ${draft.end_date}`
-                    }
-                  ]
-                }))}
-              />
+            <Col xs="12" md="6" lg="3">
+              <div className="oneActivity">
+                <h4>إشتراطات المباني التجارية</h4>
+                <p>
+                  <img src="/static/img/interactive/calendar.svg" alt="" />
+                  يغلق التصويت بتاريخ 01-01-2020
+                </p>
+              </div>
             </Col>
-            <Col xs="12" md="6" lg="4">
-              <CardDescription
-                header="مسودات تغلق قريباً"
-                type="b"
-                arrayOfContnt={soonCloseDrafts.slice(0, 2).map(draft => ({
-                  header: draft.title,
-                  link: `/draft-details/${draft.id}`,
-                  description: [
-                    {
-                      icon: '/static/img/Icon - most active - views Copy 3.svg',
-                      text: `يغلق التصويت بتاريخ ${draft.end_date}`
-                    }
-                  ]
-                }))}
-              />
+            <Col xs="12" md="6" lg="3">
+              <div className="oneActivity">
+                <h4>إشتراطات المباني التجارية</h4>
+                <p>
+                  <img src="/static/img/interactive/calendar.svg" alt="" />
+                  يغلق التصويت بتاريخ 01-01-2020
+                </p>
+              </div>
+            </Col>
+            <Col xs="12" md="6" lg="3">
+              <div className="oneActivity">
+                <h4>إشتراطات المباني التجارية</h4>
+                <p>
+                  <img src="/static/img/interactive/calendar.svg" alt="" />
+                  يغلق التصويت بتاريخ 01-01-2020
+                </p>
+              </div>
+            </Col>
+            <Col xs="12" md="6" lg="3">
+              <div className="oneActivity">
+                <h4>إشتراطات المباني التجارية</h4>
+                <p>
+                  <img src="/static/img/interactive/calendar.svg" alt="" />
+                  يغلق التصويت بتاريخ 01-01-2020
+                </p>
+              </div>
+            </Col>
+            <Col xs="12" md="6" lg="3">
+              <div className="oneActivity">
+                <h4>إشتراطات المباني التجارية</h4>
+                <p>
+                  <img src="/static/img/interactive/calendar.svg" alt="" />
+                  يغلق التصويت بتاريخ 01-01-2020
+                </p>
+              </div>
+            </Col>
+            <Col xs="12" md="6" lg="3">
+              <div className="oneActivity">
+                <h4>إشتراطات المباني التجارية</h4>
+                <p>
+                  <img src="/static/img/interactive/calendar.svg" alt="" />
+                  يغلق التصويت بتاريخ 01-01-2020
+                </p>
+              </div>
             </Col>
           </Row>
           <div className="text-center">
             <Link href="/drafts">
               <Button outline color="primary" size="md">
                 كل المسودات
+                <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
               </Button>
             </Link>
           </div>
         </Container>
       </section>
-
+      <section className="social-sharing p100">
+        <Container>
+          <h2 className="header">المشاركة المجتمعية</h2>
+          <h5>من الأكثر تأثيراً؟</h5>
+          <Row>
+            <Col md="4">
+              <div className="topSingle  d-flex flex-row">
+                <img
+                  src="/static/img/interactive/user.svg"
+                  alt=""
+                  className="avatar"
+                />
+                <div className="singleName">
+                  <p>اسم المستخدم</p>
+                  <span> 1280 نقطة</span>
+                </div>
+                <div className="trophy d-flex align-items-center">
+                  <img src="/static/img/interactive/trophy.svg" alt="" />
+                  <span>1280</span>
+                </div>
+              </div>
+            </Col>
+            <Col md="4">
+              <div className="topSingle  d-flex flex-row">
+                <img
+                  src="/static/img/interactive/user.svg"
+                  alt=""
+                  className="avatar"
+                />
+                <div className="singleName">
+                  <p>اسم المستخدم</p>
+                  <span> 1280 نقطة</span>
+                </div>
+                <div className="trophy d-flex align-items-center">
+                  <img src="/static/img/interactive/trophy.svg" alt="" />
+                  <span>1280</span>
+                </div>
+              </div>
+            </Col>
+            <Col md="4" className="">
+              <div className="topSingle  d-flex flex-row">
+                <img
+                  src="/static/img/interactive/user.svg"
+                  alt=""
+                  className="avatar"
+                />
+                <div className="singleName">
+                  <p>اسم المستخدم</p>
+                  <span> 1280 نقطة</span>
+                </div>
+                <div className="trophy d-flex align-items-center">
+                  <img src="/static/img/interactive/trophy.svg" alt="" />
+                  <span>1280</span>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="4">
+              <div className="singleConnectInfo">
+                <Row>
+                  <Col xs="4">
+                    <img src="/static/img/interactive/one.svg" alt="" />
+                  </Col>
+                  <Col xs="8">
+                    <h4>65</h4>
+                    <p>مستخدم للمنصة حاليا</p>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+            <Col md="4">
+              <div className="singleConnectInfo">
+                <Row>
+                  <Col xs="4">
+                    <img src="/static/img/interactive/report.svg" alt="" />
+                  </Col>
+                  <Col xs="8">
+                    <h4>65</h4>
+                    <p>مسودة تمت مناقشتها</p>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
       <section className="blogger">
         <Container>
-          <h2 className="text-center header">أخبار المنصة</h2>
+          <div className="d-flex justify-content-between align-items-center mb-5">
+            <h2 className="text-center header">أخبار المنصة</h2>
+            <ButtonGroup>
+              <Button className="active">أخر الأخبار</Button>
+              <Button>أهم الأخبار</Button>
+            </ButtonGroup>
+          </div>
           <Row>
-            {news.map(newsItem => (
+            <Col md="6">
+              <div className="newsSingle rightSide">
+                <img src="/static/img/interactive/news2.png" alt="" />
+                <div className="newsInfo">
+                  <h3>وزارة الشئون البلدية و القرويه تطلق مبادرة</h3>
+                  <p>
+                    تطلق غدا فاعليات مبادرة التطوع البلدي التي تنفذها وزارة
+                    الشؤون البلديه و القروية على مستوي جميع الأمانات بمختلف
+                    المناطق و المحافظات و مدن المملكة.
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col md="6">
+              <Row>
+                <Col md="6">
+                  <div className="newsSingle leftSide">
+                    <img src="/static/img/interactive/news2.png" alt="" />
+                    <div className="newsInfo">
+                      <h3>وزارة الشئون البلدية و القرويه تطلق مبادرة</h3>
+                    </div>
+                  </div>
+                </Col>
+                <Col md="6">
+                  <div className="newsSingle leftSide">
+                    <img src="/static/img/interactive/news2.png" alt="" />
+                    <div className="newsInfo">
+                      <h3>وزارة الشئون البلدية و القرويه تطلق مبادرة</h3>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col md="6">
+                  <div className="newsSingle leftSide">
+                    <img src="/static/img/interactive/news2.png" alt="" />
+                    <div className="newsInfo">
+                      <h3>وزارة الشئون البلدية و القرويه تطلق مبادرة</h3>
+                    </div>
+                  </div>
+                </Col>
+                <Col md="6">
+                  <div className="newsSingle leftSide">
+                    <img src="/static/img/interactive/news2.png" alt="" />
+                    <div className="newsInfo">
+                      <h3>وزارة الشئون البلدية و القرويه تطلق مبادرة</h3>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+            {/* {news.map(newsItem => (
               <Col key={newsItem.id} xs="12" md="6" lg="4">
                 <CardBlog
                   image={newsItem.image}
@@ -312,23 +410,28 @@ const Landing = () => {
                   tag={newsItem.tags.length > 0 && newsItem.tags[0].name}
                 />
               </Col>
-            ))}
+            ))} */}
           </Row>
           <div className="text-center">
             <Link href="/news">
               <Button outline color="primary" size="md">
                 كل الاخبار
+                <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
               </Button>
             </Link>
           </div>
         </Container>
       </section>
       <section className="args">
+        <img src="/static/img/interactive/voice2.svg" alt="" className="bg1" />
+        <img src="/static/img/interactive/voice2.svg" alt="" className="bg2" />
+
         <Container>
-          <p className="content">رأيك يهمنا…ارفع صوتك</p>
+          <p className="content">رأيك يهمنا .. صوتك يهمنا </p>
           <Link href="/drafts">
             <Button className="scale-hover" color="primary" outline size="md">
               اكتشف قائمة المسودات
+              <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
             </Button>
           </Link>
         </Container>
