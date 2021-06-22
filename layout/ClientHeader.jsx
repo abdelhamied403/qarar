5889a0582a8c3112f4e26e62b64071531cb7d0d0
ahmed.youssif <ahmed.youssif@outlook.com>
2021-06-22 11:04:06 +0200

    login

--- layout/ClientHeader.jsx
+++ layout/ClientHeader.jsx
@@ -263,26 +263,26 @@ class ClientHeader extends React.Component {
                     </Link>
                   </NavItem>
                   {!isAuthentcated && (
-                    // <NavItem
-                    //   active={
-                    //     router.pathname ===
-                    //     'https://apps.balady.gov.sa/UsersMgmt/Login.aspx?ReturnUrl=//qarar.balady.gov.sa'
-                    //   }
-                    // >
-                    //   <Link href="https://apps.balady.gov.sa/UsersMgmt/Login.aspx?ReturnUrl=//qarar.balady.gov.sa">
-                    //     <a>
-                    //       <NavLink onClick={this.closeMobile}> دخول</NavLink>
-                    //     </a>
-                    //   </Link>
-                    // </NavItem>
-
-                    <NavItem active={router.pathname === '/login'}>
-                      <Link href="/login">
+                    <NavItem
+                      active={
+                        router.pathname ===
+                        'https://apps.balady.gov.sa/UsersMgmt/Login.aspx?ReturnUrl=//qarar.balady.gov.sa'
+                      }
+                    >
+                      <Link href="https://apps.balady.gov.sa/UsersMgmt/Login.aspx?ReturnUrl=//qarar.balady.gov.sa">
                         <a>
-                          <NavLink onClick={this.closeMobile}>دخول</NavLink>
+                          <NavLink onClick={this.closeMobile}> دخول</NavLink>
                         </a>
                       </Link>
                     </NavItem>
+
+                    // <NavItem active={router.pathname === '/login'}>
+                    //   <Link href="/login">
+                    //     <a>
+                    //       <NavLink onClick={this.closeMobile}>دخول</NavLink>
+                    //     </a>
+                    //   </Link>
+                    // </NavItem>
                   )}
 
                   {isAuthentcated ? this.userDropdown() : ''}
